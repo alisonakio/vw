bb3c181c6d6786ce2b7994532417b9c9b35d400c
Alison Akio Paulo Miazaki <alison.miazaki@gmail.com>
2017-01-20 15:38:27 -0200

    project structure

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 var config = require('../config');
 var app = require('./server');
-var http = require('http').Server(app);
 
-http.listen(config.port,function(){
+app.listen(config.port,function(){
 	console.log('Listening on %d...', config.port);
 });
